2d0f9a70931832b55bbf7a0cfb0632fca18f0d20
georgedevasia0 <georgedevasia0@george.github.com>
2020-05-28 16:45:12 +0530

    updated
    updated

--- printer/webapp/views.py
+++ printer/webapp/views.py
@@ -3,10 +3,8 @@ from django.http import HttpResponse
 import os
 import json
 from webapp.models import product,services,brands
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 def index(request):
-    file=os.path.join(BASE_DIR,'/static/main.json')
-    f = open (file ,"r")
+    f = open ('static/main.json', "r")
     info=json.loads(f.read())
     products=product.objects.all()
     allservices=services.objects.all()
